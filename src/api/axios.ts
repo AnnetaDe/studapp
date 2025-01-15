b568925886b5498ece3a564f2c070ba66603fe06
AnnetaDe <anneta.liss@gmail.com>
2025-01-15 23:17:00 +0100

    upd

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,19 +1,83 @@
 import type { CreateAxiosDefaults } from 'axios';
 import axios from 'axios';
+import Cookies from 'js-cookie';
+import { ResponseCookies } from 'next/dist/compiled/@edge-runtime/cookies';
+
 
 
 
 const axiosAuthOptions: CreateAxiosDefaults = {
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
+	withCredentials: true,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 };
 
 const axiosOptions: CreateAxiosDefaults = {
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+	baseURL: process.env.NEXT_PUBLIC_API_URL,
+	
 };
 
 export const axiosNoAuth = axios.create(axiosOptions);
 export const axiosAuth = axios.create(axiosAuthOptions);
+axiosNoAuth.interceptors.request.use(config => {
+	console.log('request', config);
+	return config;
+}
+);
+axiosNoAuth.interceptors.response.use(async response => {
+	console.log('response', response);
+	const setCookieHeader = response.headers['set-cookie'];
+	
+
+	const authorization = response.headers['authorization'];
+	const cook=response.headers['Set-Cookie']
+	console.log('authorization', authorization, cook);
+
+
+	return response;
+});
+
+const saveTokenStorage = (accessToken: string) => {
+	Cookies.set('access_token', accessToken, {
+		domain: '192.168.1.73:3000',
+		expires: 1 / 24,
+		sameSite: 'none',
+		secure: true,
+	});
+};
+
+const removeFromStorage = () => {
+	Cookies.remove('access_token');
+};
+// axiosAuth.interceptors.request.use(
+// 	async config => {
+// 		let token = Cookies.get('access_token');
+// 		if (token) {
+// 			config.headers.Authorization = `Bearer ${token}`;
+// 		}
+// 		return config;
+// 	},
+// 	error => {
+// 		return Promise.reject(error);
+// 	}
+// );
+// axiosAuth.interceptors.response.use(
+// 	config => config,
+// 	async error => {
+// 		const originalRequest = error.config;
+// 		if (error.response?.status === 401 && !originalRequest._retry) {
+// 			originalRequest._retry = true;
+// 			try {
+// 				const response = await axiosNoAuth.post('/auth/refresh');
+// 				const token = response.data.access_token;
+// 				Cookies.set('access_token', token);
+// 				return axiosNoAuth(originalRequest);
+// 			} catch (error) {
+// 				console.log('Error refreshing token:', error);
+// 			}
+// 		}
+// 		return Promise.reject(error);
+// 	}
+// );
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,61 +1,61 @@
 import { axiosAuth, axiosNoAuth } from '@/api/axios';
 import { API_ENDPOINTS } from '@/api/endpoints';
 import type { AxiosResponse } from 'axios';
-import Cookies from 'js-cookie';
-
 
 export interface IAuthFormData {
-  username: string;
-  password: string;
-  name?: string;
+	username: string;
+	password: string;
+	name?: string;
 }
 
 class AuthService {
-  async login(data: IAuthFormData): Promise<AxiosResponse> {
-    try {
-      const formData: URLSearchParams = new URLSearchParams();
-      formData.append('username', data.username);
-      formData.append('password', data.password);
-      const response = await axiosNoAuth.post(API_ENDPOINTS.LOGIN, formData);
-      
+	async login(data: IAuthFormData): Promise<AxiosResponse> {
+		try {
+			const formData: URLSearchParams = new URLSearchParams();
+			formData.append('username', data.username);
+			formData.append('password', data.password);
+			const response = await axiosNoAuth.post(API_ENDPOINTS.LOGIN, formData, {
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					withCredentials: true,
+				},
+			});
 
-      return response;
-    } catch (error) {
-      console.error('Error fetching login:', error);
-      throw error;
-    }
-  }
+			return response;
+		} catch (error) {
+			console.error('Error fetching login:', error);
+			throw error;
+		}
+	}
 
-  async register(data: IAuthFormData) {
-    try {
-      const rdata = {
-        username: data.username,
-        password: data.password,
-        name: data.name,
-      };
-      return axiosNoAuth.post(API_ENDPOINTS.REGISTER, rdata);
-    } catch (error) {
-      console.error('Error fetching register:', error);
-      throw error;
-    }
-  }
+	async register(data: IAuthFormData) {
+		try {
+			const rdata = {
+				username: data.username,
+				password: data.password,
+				name: data.name,
+			};
+			return axiosNoAuth.post(API_ENDPOINTS.REGISTER, rdata);
+		} catch (error) {
+			console.error('Error fetching register:', error);
+			throw error;
+		}
+	}
 
-  async logout() {
-    
-    localStorage.removeItem('refresh_token');
-    
-    
-    return axiosAuth.post(API_ENDPOINTS.LOGOUT);
-  }
-  async refresh() {
-    try {
-      const refresh_token = localStorage.getItem('refresh_token');
-      return axiosNoAuth.post(API_ENDPOINTS.REFRESH);
-    } catch (error) {
-      throw error;
-    }
-  }
-  async profile() {
+	async logout() {
+		localStorage.removeItem('refresh_token');
+
+		return axiosAuth.post(API_ENDPOINTS.LOGOUT);
+	}
+	async refresh() {
+		try {
+			const refresh_token = localStorage.getItem('refresh_token');
+			return axiosNoAuth.post(API_ENDPOINTS.REFRESH);
+		} catch (error) {
+			throw error;
+		}
+	}
+	async profile() {
 		try {
 			const response = await axiosAuth.get(API_ENDPOINTS.PROFILE);
 			return response.data;
