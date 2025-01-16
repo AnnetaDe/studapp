e5b34437a484bf70c61fe3f268738521a13ddb7a
AnnetaDe <anneta.liss@gmail.com>
2025-01-16 12:23:15 +0100

    Update page.tsx

--- src/app/(public)/awards/page.tsx
+++ src/app/(public)/awards/page.tsx
@@ -1,13 +1,21 @@
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useQuery } from '@tanstack/react-query';
 import type { Metadata } from 'next';
 import Link from 'next/link';
+import { authService } from '../../../services/auth.service';
 
 export const metadata: Metadata = {
 	title: '',
 };
 
 export default function Page() {
+	const { data, error, isLoading } = useQuery({
+		queryKey: ['profile'],
+		queryFn: () => authService.profile(),
+	});
+	console.log(data);
+
 	return (
 		<div className="flex flex-col justify-center">
 			oops nothing here ...
