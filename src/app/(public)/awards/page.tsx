46b22d6b07391b7fc8ca523975aab64a54b881db
AnnetaDe <anneta.liss@gmail.com>
2025-01-16 12:27:15 +0100

    Update page.tsx

--- src/app/(public)/awards/page.tsx
+++ src/app/(public)/awards/page.tsx
@@ -1,20 +1,14 @@
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useQuery } from '@tanstack/react-query';
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import { authService } from '../../../services/auth.service';
 
 export const metadata: Metadata = {
 	title: '',
 };
 
 export default function Page() {
-	const { data, error, isLoading } = useQuery({
-		queryKey: ['profile'],
-		queryFn: () => authService.profile(),
-	});
-	console.log(data);
+
 
 	return (
 		<div className="flex flex-col justify-center">
